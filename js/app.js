7775f735dca7b3135b497860e7bc01906e6cbcc5
amy_pr <ampreci@gmail.com>
2023-07-15 18:11:39 -0700

    fetch the list of breeds data from the api and display as selector options

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ const form = document.querySelector('form');
 // ------------------------------------------
 
 
+fetch('https://dog.ceo/api/breeds/list')
+  .then(response => response.json())
+  .then(data => generateOptions(data.message)) //data returned gives us an array with the name of each breed as a string
+
 fetch('https://dog.ceo/api/breeds/image/random')
     //.then(response=>console.log(response)) //returns a response object that lets us know the status (pass = 200). The actual content is in the body property. To access the data, need to parse to JSON
     .then(response => response.json())//returns raw data in json format; returns a promise
@@ -16,6 +20,14 @@ fetch('https://dog.ceo/api/breeds/image/random')
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
+function generateOptions(data) {
+    const options = data.map(item => `
+      <option value='${item}'>${item}</option>
+  `).join(''); //used join to get rid of the commas that were in the array
+    select.innerHTML = options; 
+  }
+
 
 function generateImage(data) { //create a template literal and insert the value of the data using interpolation
     const html = ` 
